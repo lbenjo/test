087513375d4ca02478b6275a16e293af313c00e8
lbenjo <>
2024-07-31 18:22:59 +0300

    Update 1
    +join and disconnect messages
    +i mostly tried to understand communication between client and server.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 var server = app.listen(3000);
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
 var path = require("path");
 const { randomUUID } = require("crypto");
 
@@ -10,28 +10,57 @@ var io = new Server(server);
 
 var clients = [];
 
-app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.sendFile(__dirname + '/htmls/main.html');
 });
 
 
-io.on('connection', (socket)=>{
+
+io.on('connection', (socket) => {
 
     var userUUID = randomUUID();
+    var _userName = null;
+    socket.emit("setUUID", userUUID);
 
-    socket.emit("setUUID",userUUID);
-    console.log('a user connected (' + userUUID + ')');
-    
-    socket.on("disconnect",()=>{
-        console.log('user disconnected');
+    socket.on("disconnect", () => {
+
+        console.log(clients);
+
+       for(let i = 0; i < clients.length; i++) {
+
+            if (clients[i]["UUID"] == userUUID) {
+                clients.splice(i,1);
+                console.log(_userName + " has disconnected!");
+                socket.broadcast.emit("s_send_message","Server", _userName + " has disconnected!");
+            }
+        }
+
+        console.log(clients);
+
     });
 
-    socket.on("message_sent", (name,msg)=>{
+    socket.on("message_sent", (name, msg) => {
         console.log("[" + name + "]: " + msg);
-        socket.broadcast.emit("s_send_message",name,msg);
+        socket.broadcast.emit("s_send_message", name, msg);
     });
+
+
+
+    socket.on("onJoin", (userName) => {
+        _userName = userName
+
+        clients.push({
+            "UUID": userUUID,
+            "username": userName,
+        });
+
+        console.log(userName + " has connected!");
+        socket.broadcast.emit("s_send_message", "Server", userName + " has connected!");
+    });
+
+    socket.emit("connection_success");
 });
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,11 @@
-var socket = io.connect();
 
+var socket = null;
 var clientUUID = null;
+var userName = "";
 
 var nameText = document.getElementById("nameText");
+var enterGameButton = document.getElementById("enterGame");
+
 var chatText = document.getElementById("chatText");
 var sendButton = document.getElementById("sendButton");
 var chatBox = document.getElementById("chatBox");
@@ -11,25 +14,47 @@ function addMessage(name,msg){
     chatBox.innerHTML = chatBox.innerHTML + '<div class="messageBox">' + '[' + name + ']: ' + msg + '</div>'
 }
 
-sendButton.onclick = ()=>{
+function Connect(){
 
-
-    var name = nameText.value;
-    var message = chatText.value;
-    if (name == "") {console.warn("You should write your nickname!"); return;}
+    if (nameText.value == "") {console.warn("You should write your nickname!"); return;}
 
     
-    console.log("[" + name + "]: " + message);
+    userName = nameText.value;
+    nameText.style = "display:none;";
+    enterGameButton.style = "display:none;";
+    socket = io.connect();
 
-    addMessage(name,message);
-    socket.emit("message_sent", name, message);
+    socket.on("connection_success",()=>{
+
+        //ADDS MESSAGE TO CHAT
+
+        socket.on("s_send_message", (name,msg)=>{
+            addMessage(name,msg);
+            console.log("[" + name + "]: " + msg);
+        });
+        
+        socket.on("setUUID", (uuid)=>{
+            clientUUID = uuid;
+        });
+
+    
+        socket.emit("onJoin",userName);
+        addMessage("Client","You are successfully connected!");
+
+        chatText.style = "display: flex;";
+    })
+    
+
+}
+
+
+sendButton.onclick = ()=>{
+
+    var message = chatText.value;
+
+    console.log("[" + userName + "]: " + message);
+
+    addMessage(userName,message);
+    socket.emit("message_sent", userName, message);
 };
 
-socket.on("s_send_message", (name,msg)=>{
-    addMessage(name,msg);
-    console.log("[" + name + "]: " + msg);
-});
-
-socket.on("setUUID", (uuid)=>{
-    clientUUID = uuid;
-});
